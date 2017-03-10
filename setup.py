0ae47f7038734d5d6c2614042b9cc0d4cff6d71b
James Boehmer <james.boehmer@gmail.com>
2017-03-10 11:41:32 -0500

    Updated to v0.0.9.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='stratumus',
-      version='0.0.8',
+      version='0.0.9',
       description='Layered Yaml Python Configuration',
       author='James Boehmer',
       author_email='james.boehmer@gmail.com',
